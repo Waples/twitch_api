274fb78ee18551ce2bec23f116385dcd913d85ba
Florian Benscheidt <florian.benscheidt@ogd.nl>
2020-12-07 19:01:55 +0100

    blurb im to tired now

---
+++ twitch.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+TODO
+"""
+import argparse
+from twitch.modules import *
+from twitch import api
+
+p = argparse.parser(description='Description')
+p.parse_args()
+
+if __name__ == '__main__':
+    print('blurb')
